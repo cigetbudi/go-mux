0231917ccb9d62d8bef8d9e0cd1a19f65b4f364d
Sigit <cigetbudi@gmail.com>
2022-09-25 02:31:18 +0700

    error. kayak missing connection antar package

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/cigetbudi/go-mux
 go 1.19
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/lib/pq v1.10.7 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.7
 )
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"log"
+
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -41,17 +42,76 @@ func clearTable() {
 	a.DB.Exec("ALTER SEQUENCE product_id_seq RESTART WITH 1")
 }
 
-func TestEmptyTable(t *testing.T) {
-	clearTable()
+// func TestEmptyTable(t *testing.T) {
+// 	clearTable()
 
-	req, _ := http.NewRequest("GET", "/products", nil)
+// 	req, _ := http.NewRequest("GET", "/products", nil)
+// 	response := executeRequest(req)
+
+// 	checkReponseCode(t, http.StatusOK, response.Code)
+
+// 	if body := response.Body.String(); body != "[]" {
+// 		t.Errorf("salah harusnya array kosong malah dapet %s", body)
+// 	}
+// }
+
+// func TestNAProduct(t *testing.T) {
+// 	clearTable()
+
+// 	req,_ := http.NewRequest("GET", "/product/11",nil)
+// 	response := executeRequest(req)
+
+// 	checkReponseCode(t, http.StatusNotFound, response.Code)
+
+// 	var m map[string]string
+// 	json.Unmarshal(response.Body.Bytes(), &m)
+// 	if m["error"] != "Product not found" {
+// 		t.Errorf("harusnya dapet 'error' jadi 'Product not found' malah dapet '%s'", m["error"])
+// 	}
+// }
+
+// func TestCreateProduct(t *testing.T) {
+// 	clearTable()
+
+// 	var jsonStr = []byte(`{"name":"test produk", "price": 11.22}`)
+// 	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(jsonStr))
+// 	req.Header.Set("Content-Type", "application/json")
+
+// 	response := executeRequest(req)
+// 	checkReponseCode(t, http.StatusCreated, response.Code)
+
+// 	var m map[string]interface{}
+// 	json.Unmarshal(response.Body.Bytes(), &m)
+
+// 	fmt.Println(string(response.Body.Bytes()))
+
+// 	if m["name"] != "test produk" {
+// 		t.Errorf("harusnya namanya 'test produk'  malah dapet '%v'", m["name"])
+// 	}
+
+// 	if m["price"] != 11.22 {
+// 		t.Errorf("harusnya '11.2' malah dapet '%v'", m["price"])
+// 	}
+
+// 	if m["id"] != 1.0 {
+//         t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+//     }
+// }
+
+func TestGetProduct(t *testing.T) {
+	clearTable()
+	addProducts()
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
 	response := executeRequest(req)
 
 	checkReponseCode(t, http.StatusOK, response.Code)
+}
 
-	if body := response.Body.String(); body != "[]" {
-		t.Errorf("salah harusnya array kosong malah dapet %s", body)
-	}
+func addProducts() {
+
+	a.DB.Exec("INSERT INTO products(name, price) VALUES('tes', 1000.00)")
+
 }
 
 func executeRequest(req *http.Request) *httptest.ResponseRecorder {
@@ -67,8 +127,6 @@ func checkReponseCode(t *testing.T, expected, actual int) {
 	}
 }
 
-
-
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
 (
     id SERIAL,
@@ -76,5 +134,3 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
-
-
