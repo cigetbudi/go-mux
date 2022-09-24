c5f19a7c50f2da477f93117c61176269beed6670
Sigit <cigetbudi@gmail.com>
2022-09-24 23:09:30 +0700

    Testing

--- main_test.go
+++ main_test.go
@@ -1,11 +1,11 @@
 package main_test
 
 import (
+	"log"
 	"os"
 	"testing"
-	"github.com/cigetbudi/go-mux"
-	_"github.com/cigetbudi/go-mux"
 
+	_ "github.com/cigetbudi/go-mux"
 )
 
 var a main.App
@@ -16,10 +16,28 @@ func TestMain(m *testing.M) {
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"))
 
-	
+	ensureTableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
+
 }
 
 func ensureTableExists() {
-	if _, err := a.DB.Exec()
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
 }
 
+func clearTable() {
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE product_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
